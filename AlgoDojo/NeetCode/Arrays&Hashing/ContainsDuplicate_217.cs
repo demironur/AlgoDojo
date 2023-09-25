dbd5a009c20a67b97c8ffc1e55307623d3406b66
demironur <ponasoft@gmail.com>
2023-09-25 20:36:14 +0300

    217 ContainsDuplicate

---
+++ AlgoDojo/NeetCode/Arrays&Hashing/ContainsDuplicate_217.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgoDojo.NeetCode.Arrays_Hashing
+{
+    //https://leetcode.com/problems/contains-duplicate/description/
+    public class ContainsDuplicate_217
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> numsHash = new HashSet<int>();
+
+            foreach (var item in nums)
+            {
+                if (numsHash.Contains(item))
+                    return true;
+                else
+                    numsHash.Add(item);
+            }
+
+            return false;
+        }
+    }
+}
