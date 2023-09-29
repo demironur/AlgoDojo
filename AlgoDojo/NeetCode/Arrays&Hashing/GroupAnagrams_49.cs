ce9e032818e5397b79f49f4ee877f663ef0f10e4
demironur <ponasoft@gmail.com>
2023-09-29 13:38:52 +0300

    49 GroupAnagrams

---
+++ AlgoDojo/NeetCode/Arrays&Hashing/GroupAnagrams_49.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgoDojo.NeetCode.Arrays_Hashing
+{
+    //https://leetcode.com/problems/group-anagrams/
+    public class GroupAnagrams_49
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var groupedStrings = new Dictionary<string, IList<string>>() { };
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                var key = Core.GetStringHashKey(strs[i]);
+
+                if (!groupedStrings.ContainsKey(key))
+                    groupedStrings.Add(key, new List<string>() { strs[i] });
+                else
+                    groupedStrings[key].Add(strs[i]);
+            }
+
+            return groupedStrings.Values.ToList();
+        }
+    }
+}
