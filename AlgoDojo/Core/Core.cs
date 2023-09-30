b3fa87082c23190d39ed6fca2eb12b512812825a
demironur <ponasoft@gmail.com>
2023-09-30 11:11:36 +0300

    128 LongestConsecutiveSequence

---
+++ AlgoDojo/Core/Core.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgoDojo
+{
+    public static class Core
+    {
+        public static string GetStringHashKey(string s)
+        {
+            var key = new char[26];
+            foreach (var letter in s)
+                key[letter - 'a']++;
+            return new string(key);
+        }
+    }
+}
---
+++ AlgoDojo/NeetCode/Arrays&Hashing/LongestConsecutiveSequence_128.cs
@@ -0,0 +1,34 @@
+﻿namespace AlgoDojo.NeetCode.Arrays_Hashing
+{
+    //https://leetcode.com/problems/longest-consecutive-sequence/
+    public class LongestConsecutiveSequence_128
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+
+            HashSet<int> numsSet = nums.ToHashSet();
+            int longest = 0;
+
+            foreach (var item in numsSet)
+            {
+                if (!numsSet.Contains(item - 1))
+                {
+                    var index = item + 1;
+                    int tempLongest = 1;
+
+                    while (numsSet.Contains(index))
+                    {
+                        tempLongest++;
+                        index++;
+                    }
+
+                    longest = Math.Max(longest, tempLongest);
+                }
+            }
+
+            return longest;
+        }
+    }
+}
