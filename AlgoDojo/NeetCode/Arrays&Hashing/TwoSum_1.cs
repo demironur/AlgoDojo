9f641e4ed851ff16f40ac3aaa438b567c1e86fba
demironur <ponasoft@gmail.com>
2023-11-10 15:44:33 +0300

    1 TwoSum

---
+++ AlgoDojo/NeetCode/Arrays&Hashing/TwoSum_1.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgoDojo.NeetCode.Arrays_Hashing
+{
+    //https://leetcode.com/problems/two-sum/ 
+    public class TwoSum_1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var numsDic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var diff = target - nums[i];
+
+                if (numsDic.ContainsKey(diff))
+                    return new int[] { i, numsDic[diff] };
+
+                numsDic[nums[i]] = i;
+            }
+
+            return null;
+        }
+    }
+}
