0d1f2d33bfe3323c5b28380e445434e5f8574d22
demironur <ponasoft@gmail.com>
2023-11-19 15:41:45 +0300

    36 ValidSudoku

---
+++ AlgoDojo/NeetCode/Arrays&Hashing/ValidSudoku_36.cs
@@ -0,0 +1,45 @@
+﻿namespace AlgoDojo.NeetCode.Arrays_Hashing
+{
+    //https://leetcode.com/problems/valid-sudoku/
+    public class ValidSudoku_36
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            List<HashSet<char>> columnHash = new List<HashSet<char>>();
+            List<HashSet<char>> rowHash = new List<HashSet<char>>();
+            List<HashSet<char>> squareHash = new List<HashSet<char>>();
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                columnHash.Add(new HashSet<char>());
+                rowHash.Add(new HashSet<char>());
+                squareHash.Add(new HashSet<char>());
+            }
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    char cell = board[i][j];
+
+                    if (cell == '.')
+                        continue;
+                    int sIndex = 3 * (i / 3) + j / 3;
+
+                    if (columnHash[i].Contains(cell) || rowHash[j].Contains(cell) || squareHash[sIndex].Contains(cell))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        columnHash[i].Add(cell);
+                        rowHash[j].Add(cell);
+                        squareHash[sIndex].Add(cell);
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
